c8ccf317e563b34f3587cc000a02bad69d21e770
chimaro <chimaegwoum@gmail.com>
2022-12-08 15:43:54 -0800

    4-puts

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,11 @@
 #include <stdio.h>                                                                                                             
 /**                                                                                                                            
- *  * main - Entry                                                                                                                
- *   * Return: Always 0                                                                                                            
- *    */                                                                                                                            
-int main(void)                                                                                                                 
-{                                                                                                                              
-	        printf("with proper grammar, but the outcome is a piece of art,\n");                                                   
-		        return (0);                                                                                                            
+*  main - main block
+*  Return: o
+* /
+int main(void)
+{
+	puts("\ "programming is like building a multilingual puzzle");
+	return (0);
 }
+		
