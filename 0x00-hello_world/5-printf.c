bb013cb4b74ec1f1accae23912fa2d35628805c0
chimaro <chimaegwoum@gmail.com>
2022-12-09 04:50:48 -0800

    Printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -6,11 +6,11 @@
  *
  *  * main - Entry point
  *
- *  *
+ *   *
  *
- *  * Return: Always 0 (success)
+ *    * Return: Always 0 (success)
  *
- *  */
+ *     */
 
 int main(void)
 
@@ -18,7 +18,7 @@ int main(void)
 
 		printf("with proper grammar, but the outcome is a piece of art,\n");
 
-		return (0);
+			return (0);
 
 
 
