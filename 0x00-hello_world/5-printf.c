166de2c466721fff8cd59592318f4d4d95160e19
chimaro <chimaegwoum@gmail.com>
2022-12-08 16:10:41 -0800

    5-print

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,11 @@
-#include <stdio.h>                                                                                                             
-/**                                                                                                                            
-*  main - main block
-*  Return: o
-* /
+#include <stdio.h>                                                                                           
+/**
+ * main - Entry
+ * Return:Always 0
+ */
 int main(void)
 {
-	puts("\ "programming is like building a multilingual puzzle");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
 		
